4507928afe801881bfc5e95484fc0b6e764f478d
aokiji <rcm.webdev.pro@gmail.com>
2026-05-23 11:23:40 -0700

    fix: docker does not crash on boot after seeding demo data

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 echo "Running database migrations..."
 ./node_modules/.bin/prisma migrate deploy --schema=./apps/server/prisma/schema.prisma
 echo "Seeding demo user..."
-./node_modules/.bin/prisma db seed --schema=./apps/server/prisma/schema.prisma
+node apps/server/dist/prisma/seed.js
 echo "Starting server..."
 exec node apps/server/dist/index.js
